80a856f350b85ab3f66b94909ea2e2ed7b4750e3
Italo Sergio <italosergio@graduate.org>
2022-04-20 12:42:01 -0300

    pass: requirement 5 - build Mage, Necromancer, Ranger and Warrior Archetype classes

---
+++ src/Archetypes/Necromancer.ts
@@ -0,0 +1,22 @@
+import Archetype from './Archetype';
+import { EnergyType } from '../Energy';
+
+export default class Necromancer extends Archetype {
+  private _eType: EnergyType;
+  private static _instances = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._eType = 'mana';
+  }
+
+  get energyType() {
+    return this._eType;
+  }
+
+  static createdArchetypeInstances() {
+    this._instances += 1;
+   
+    return this._instances;
+  }
+}
