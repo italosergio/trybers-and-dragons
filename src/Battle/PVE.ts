59322ba4e226247aa5ca87917c9a6ad714aed86b
Italo Sergio <italosergio@graduate.org>
2022-04-23 14:59:43 -0300

    pass: requirement 11 - build PVE class

---
+++ src/Battle/PVE.ts
@@ -0,0 +1,20 @@
+import Fighter, { SimpleFighter } from '../Fighter';
+import Battle from './Battle';
+
+export default class PVE extends Battle {
+  private environment: (SimpleFighter | Fighter)[];
+
+  constructor(fighter: Fighter, environment: (SimpleFighter | Fighter)[]) {
+    super(fighter);
+    this.environment = environment;
+  }
+
+  fight(): number {
+    this.environment.forEach((enemy) => {
+      enemy.attack(this.player);
+      this.player.attack(enemy);
+    });
+
+    return this.player.lifePoints === -1 ? -1 : 1;
+  }
+}
