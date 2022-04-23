c395fdebe1bf7733780643f652277e506b7ff7d0
Italo Sergio <italosergio@graduate.org>
2022-04-23 17:07:11 -0300

    pass: requirement 13 - build T&D battles

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import Character from './Character';
+import Dragon from './Dragon';
+import Monster from './Monster';
+import Battle, { PVP, PVE } from './Battle';
+
+const player1 = new Character('');
+const player2 = new Character('');
+const player3 = new Character('');
+
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
+
+const monster1 = new Monster();
+const monster2 = new Dragon();
+
+const pvp = new PVP(player2, player3);
+const pve = new PVE(player1, [monster1, monster2]);
+
+function runBattles(battles: Battle[]):void {
+  battles.forEach((battle) => battle.fight());
+}
+
+export { 
+  player1, 
+  player2, 
+  player3, 
+  monster1, 
+  monster2,
+  pvp,
+  pve,
+  runBattles,
+};
